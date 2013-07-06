bae7b595863764a0356d01017b9f69ff5f67e23b
Kyle Hickson <kylehickson@hotmail.co.uk>
2013-07-06 17:07:57 +0100

    layout...

---
+++ layout.lua
@@ -0,0 +1,11 @@
+local Building = {}
+Building.Name = "building name"
+Building.ID =  -- last one was 4 so make this 5
+Building.Ownable = false
+Building.Govenment = false
+Building.Price = 0
+Building.Doors = {
+	-- vector table
+}
+
+Property:CreateNew(Building)
