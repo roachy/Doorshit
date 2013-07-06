e2d6c2a6f86cde47558f14aeabb8eaf0432778f6
Kyle Hickson <kylehickson@hotmail.co.uk>
2013-07-06 17:08:55 +0100

    fixes

--- clothingstore.lua
+++ clothingstore.lua
@@ -5,8 +5,8 @@ Building.Ownable = true
 Building.Govenment = false
 Building.Price = 0
 Building.Doors = {
-	Vector(-5201.21484375, -6466.0131835938, 79.729568481445) // First Door
-	Vector(-5281.380859375, -6467.0283203125, 75.111602783203)} // Second Door
+	{Vector(-5201.21484375, -6466.0131835938, 79.729568481445)}, // First Door
+	{Vector(-5281.380859375, -6467.0283203125, 75.111602783203)} // Second Door
 	
 }
 
