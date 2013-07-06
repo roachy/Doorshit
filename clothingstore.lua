192f8e5e93370e0802af6303e3d457dc1459d39e
roachy <roach_trivium@hotmail.co.uk>
2013-07-06 17:06:58 +0100

    Created the Clothing store

---
+++ clothingstore.lua
@@ -0,0 +1,13 @@
+local Building = {}
+Building.Name = "Clothing Store"
+Building.ID =  5
+Building.Ownable = true
+Building.Govenment = false
+Building.Price = 0
+Building.Doors = {
+	Vector(-5201.21484375, -6466.0131835938, 79.729568481445) // First Door
+	Vector(-5281.380859375, -6467.0283203125, 75.111602783203)} // Second Door
+	
+}
+
+Property:CreateNew(Building)
